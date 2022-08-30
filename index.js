4315f33bc6fafb9ccc1219125336add40295c64b
trevor dewindt <trevor.dewindt@gmail.com>
2022-08-30 16:37:01 -0600

    commit tree 744873e8ae2ab088e7b53fd8b0e5921ed295bf65

---
+++ index.js
@@ -0,0 +1,16 @@
+const myArray = [1,2,4,5,6,'seven'];
+
+const myObject = {
+    name: 'Tim',
+    age: 25,
+    hobbies: ['video games', 'sports', 'food'],
+};
+
+
+function myEach(collection, callback){
+   Object.values(collection).map()
+}   
+
+
+
+console.log(myEach(myObject))//, mapper))
